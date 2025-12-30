7ecdb76d8da5210bb3a8964b421f95d4a7d14457
vertiljivenson9 <vertiljivenson9@gmail.com>
2025-12-30 19:11:33 -0400

    retroOS Neural Mesh Sync: SoundService.ts

--- services/SoundService.ts
+++ services/SoundService.ts
@@ -1,8 +1,8 @@
 class SoundService {
   private static instance: SoundService;
-  public static getInstance(): SoundService { if (!SoundService.instance) SoundService.instance = new SoundService(); return SoundService.instance; }
-  public playClick() { new Audio("https://actions.google.com/sounds/v1/foley/mechanical_switch_single_click.ogg").play().catch(() => {}); }
-  public playAppOpen() { new Audio("https://actions.google.com/sounds/v1/science_fiction/beep_acknowledge.ogg").play().catch(() => {}); }
-  public playError() { new Audio("https://actions.google.com/sounds/v1/alarms/beep_short.ogg").play().catch(() => {}); }
+  public static getInstance() { if(!SoundService.instance) SoundService.instance = new SoundService(); return SoundService.instance; }
+  public playClick() { new Audio("https://actions.google.com/sounds/v1/foley/mechanical_switch_single_click.ogg").play().catch(()=>{}); }
+  public playAppOpen() { new Audio("https://actions.google.com/sounds/v1/science_fiction/beep_acknowledge.ogg").play().catch(()=>{}); }
+  public playError() { new Audio("https://actions.google.com/sounds/v1/alarms/beep_short.ogg").play().catch(()=>{}); }
 }
 export const soundService = SoundService.getInstance();
