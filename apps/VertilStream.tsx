f88e63a92e9f6beda63d475a2beda2824e673c4b
vertiljivenson9 <vertiljivenson9@gmail.com>
2025-12-30 20:03:00 -0400

    retroOS Neural Mesh Sync: VertilStream.tsx

--- apps/VertilStream.tsx
+++ apps/VertilStream.tsx
@@ -4,9 +4,9 @@ export const VertilStream = () => {
   return (
     <div className="h-full flex flex-col bg-zinc-950">
       <div className="h-12 bg-black flex items-center px-4 border-b border-white/10">
-        <input value={url} onChange={e=>setUrl(e.target.value)} className="flex-1 bg-white/5 border border-white/10 rounded px-3 py-1 text-[10px] text-white/50" />
+        <input value={url} onChange={e => setUrl(e.target.value)} className="flex-1 bg-white/5 border border-white/10 rounded-lg px-3 py-1 text-[10px] text-white/50" />
       </div>
-      <iframe src={url} className="flex-1 w-full border-none bg-white" />
+      <iframe src={url} className="flex-1 w-full border-none bg-white" allow="autoplay; encrypted-media; fullscreen" />
     </div>
   );
 };
